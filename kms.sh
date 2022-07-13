9b4b71ec4c69558139873cbf586bc585a0eec14a
ICE青 <35949104+superflq@users.noreply.github.com>
2022-07-13 23:14:33 +0800

    Add files via upload

--- kms.sh
+++ kms.sh
@@ -3,18 +3,21 @@ cd /jffs/
 wget "https://github.com/superflq/kms/blob/main/vlmcsd.t"
 mv vlmcsd.t vlmcsd
 chmod 0755 vlmcsd
-/jffs/vlmcsd
-clear
+
 echo 
 netstat -an |grep 1688
 echo 
+mkdir /jffs/configs
 touch /jffs/configs/dnsmasq.conf.add
 chmod 0755 /jffs/configs/dnsmasq.conf.add
 echo "srv-host=_vlmcs._tcp.lan,`uname -n`.lan,1688,0,100">>/jffs/configs/dnsmasq.conf.add
 
+echo 
 service restart_dnsmasq
 iptables -I INPUT -p tcp --dport 1688 -j ACCEPT
+echo 
 
+mkdir /jffs/scripts
 touch /jffs/scripts/firewall-start
 chmod 0755 /jffs/scripts/firewall-start
 echo "#!/bin/sh">>/jffs/scripts/firewall-start
