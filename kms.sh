c69f66d05c16b59d8d633a31dd54535450b2c982
ICE青 <35949104+superflq@users.noreply.github.com>
2022-07-14 22:13:38 +0800

    Add files via upload

--- kms.sh
+++ kms.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 cd /jffs/
-wget "https://github.com/superflq/kms/blob/main/vlmcsd.t"
-mv vlmcsd.t vlmcsd
-chmod 0755 vlmcsd
 
-echo 
+echo
 netstat -an |grep 1688
-echo 
+
 mkdir /jffs/configs
 touch /jffs/configs/dnsmasq.conf.add
-chmod 0755 /jffs/configs/dnsmasq.conf.add
+chmod 0755 /jffs/configs/*
 echo "srv-host=_vlmcs._tcp.lan,`uname -n`.lan,1688,0,100">>/jffs/configs/dnsmasq.conf.add
 
-echo 
+echo
 service restart_dnsmasq
+echo
 iptables -I INPUT -p tcp --dport 1688 -j ACCEPT
-echo 
+
 
 mkdir /jffs/scripts
+cd /jffs/scripts
+wget "https://github.com/superflq/kms/blob/main/vlmcsd.t"
+mv vlmcsd.t vlmcsd
+
 touch /jffs/scripts/firewall-start
-chmod 0755 /jffs/scripts/firewall-start
+chmod 0755 /jffs/scripts/*
+
 echo "#!/bin/sh">>/jffs/scripts/firewall-start
-echo "/jffs/vlmcsd">>/jffs/scripts/firewall-start
+echo "/jffs/scripts/vlmcsd">>/jffs/scripts/firewall-start
 echo "service restart_dnsmasq">>/jffs/scripts/firewall-start
 echo "iptables -I INPUT -p tcp --dport 1688 -j ACCEPT">>/jffs/scripts/firewall-start
